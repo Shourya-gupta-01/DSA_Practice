b04f95c3b1c0318ffea50858c3c2d118c22a6ed1
Shourya Gupta <shouryaagupta0@gmail.com>
2026-05-09 21:45:44 +0530

    Find missing Number

--- Striver_array_easy.py
+++ Striver_array_easy.py
@@ -74,18 +74,19 @@ def move_0_end(arr):
         
             slow += 1
 
+# Linear Search
 def linear_search(arr, find):
     for i in range(len(arr)):
         if arr[i] == find:
             return i
     return 'Not Found'
 
+# Union of Two Sorted Arrays
 def union_of_two_sorted_arrays(arr1, arr2):
     i, j = 0, 0
     res = []
     
     while i < len(arr1) and j < len(arr2):
-        # Pick the smaller element to maintain sorted order
         if arr1[i] <= arr2[j]:
             val = arr1[i]
             i += 1
@@ -93,28 +94,35 @@ def union_of_two_sorted_arrays(arr1, arr2):
             val = arr2[j]
             j += 1
         
-        # Only append if res is empty or val is not a duplicate
         if not res or res[-1] != val:
             res.append(val)
 
-    # Handle remaining elements in arr1
     while i < len(arr1):
         if res[-1] != arr1[i]:
             res.append(arr1[i])
         i += 1
 
-    # Handle remaining elements in arr2
     while j < len(arr2):
         if res[-1] != arr2[j]:
             res.append(arr2[j])
         j += 1
         
     return res
-            
+
+# Find Missing Number
+def missing_num(arr):
+    n = len(arr) + 1
+    arr_sum = 0
+    summ = n * ((n + 1) / 2)
+
+    for i in range(len(arr)):
+        arr_sum += arr[i]
+
+    return summ - arr_sum
 
 
 if __name__ == '__main__':
-    arr = [1, 0, 5, 0, 65, 0, 0, 98, 76, 43]
+    arr = [1, 2, 3, 4, 5, 6, 7]
     arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     arr2 = [2, 3, 4, 4, 5, 11, 12]
     # print(f'The Largest element in the array is : {largest_element(arr)}')
@@ -130,4 +138,5 @@ if __name__ == '__main__':
     # move_0_end(arr)
     # print(f'The array where 0 are in end : {arr}')
     # print(f'5 is located at : {linear_search(arr, 4)}')
-    print(f'The union of two arrays is : {union_of_two_sorted_arrays(arr1, arr2)}')
+    # print(f'The union of two arrays is : {union_of_two_sorted_arrays(arr1, arr2)}')
+    print(f'The Missing number in the array is : {missing_num(arr)}')
