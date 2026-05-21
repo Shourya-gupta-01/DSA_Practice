591812377bfd1b773bf7eed2f3c30edb0df18429
Shourya Gupta <shouryaagupta0@gmail.com>
2026-05-21 08:12:05 +0530

    Majority Element 2

--- Striver_array_hard.py
+++ Striver_array_hard.py
@@ -23,5 +23,26 @@ def pascalTriangle(numRows):
 
     return dummy
 
+# Majority Element 2 --> Boyer Moore Algorithim
+def majorityElement(nums):
+    count1 = count2 = 0
+    cand1 = cand2 = None
+    for num in nums:
+        if num == cand1:
+            count1 += 1
+        elif num == cand2:
+            count2 += 1
+        elif count1 == 0:
+            cand1, count1 = num, 1
+        elif count2 == 0:
+            cand2, count2 = num, 1
+        else:
+            count1 -= 1
+            count2 -= 1
+    return [n for n in (cand1, cand2) if nums.count(n) > len(nums) // 3]
+
 if __name__ == "__main__":
-    print(f'The Pascal Triangle {pascalTriangle(4)}')
+    arr = [11, 33, 33, 11, 33, 11]
+    # print(f'The Pascal Triangle {pascalTriangle(4)}')
+    ans = majorityElement(arr)
+    print("The majority elements are:", ans)
