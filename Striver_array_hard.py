cc278de6d620b395bbf3ac06f6eac1bdd7fa6ee4
Shourya Gupta <shouryaagupta0@gmail.com>
2026-05-25 13:58:18 +0530

    4Sum

--- Striver_array_hard.py
+++ Striver_array_hard.py
@@ -63,9 +63,40 @@ def threeSum(nums):
                 i+=1
     return ans
 
+# 4Sum
+def fourSum(arr, target):
+    n = len(arr)
+    arr.sort()
+    ans = []
+
+    for i in range(n):
+        if i > 0 and arr[i] == arr[i - 1]:
+            continue
+
+        for j in range(i + 1, n):
+            if j > i + 1 and arr[j] == arr[j - 1]:
+                continue
+
+            left, right = j + 1, n - 1
+            while left < right:
+                total = arr[i] + arr[j] + arr[left] + arr[right]
+                if total == target:
+                    ans.append([arr[i], arr[j], arr[left], arr[right]])
+                    while left < right and arr[left] == arr[left + 1]:
+                        left += 1
+                    while left < right and arr[right] == arr[right - 1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
+                elif total < target:
+                    left += 1
+                else:
+                    right -= 1
+    return ans
+
 if __name__ == "__main__":
-    arr = [-1, 0, 1, 2, -1, -4]
+    arr = [1,0,-1,0,-2,2]
     # print(f'The Pascal Triangle {pascalTriangle(4)}')
     # ans = majorityElement(arr)
     # print("The majority elements are:", ans)
-    print(f'The target sum is of : {threeSum(arr)}')
+    print(f'The target sum is of : {fourSum(arr, 0)}')
