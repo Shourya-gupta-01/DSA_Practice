b90b4a13bfcb29e550169002a78232060de639cd
Shourya Gupta <shouryaagupta0@gmail.com>
2026-06-04 12:49:50 +0530

    Search Single Element in sorted array

--- Striver_1DBS.py
+++ Striver_1DBS.py
@@ -181,6 +181,25 @@ def countRotate(arr):
             right = mid - 1
     return left
 
+# Search Single Element in sorted array
+def singleNonDuplicate(nums: list[int]) -> int:
+    if len(nums) == 1:
+        return nums[0]
+    if nums[0] != nums[1]:
+        return nums[0]
+    if nums[-1] != nums[-2]:
+        return nums[-1]
+    left = 1
+    right = len(nums) - 2
+    while left <= right:
+        mid = left + (right - left) // 2
+        if (nums[mid] != nums[mid - 1]) and (nums[mid] != nums[mid + 1]):
+            return nums[mid]
+        if (mid % 2 == 0 and nums[mid] == nums[mid + 1]) or (mid % 2 == 1 and nums[mid] == nums[mid - 1]):
+            left = mid + 1
+        else:
+            right = mid - 1
+
 if __name__ == '__main__':
-    arr = [3, 4, 5, 1, 2]
-    print(f'The element x found at index: {countRotate(arr)}')
+    arr = [1,2,2,3,3,4,4,8,8]
+    print(f'The single occured element is: {singleNonDuplicate(arr)}')
