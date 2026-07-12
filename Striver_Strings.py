cf105903220f4202b2ef39501b2c4ab2e87673fb
Shourya Gupta <shouryaagupta0@gmail.com>
2026-07-12 14:31:00 +0530

    Longest Palindromic Substring

--- Striver_Strings.py
+++ Striver_Strings.py
@@ -145,6 +145,28 @@ def count_sustrings(s, k):
 
     return atMostKDistincts(s, k) - atMostKDistincts(s, k - 1)
 
+# Longest Palindromic Substring
+def longestPalindrome(s):
+    res=""
+    cnt=0
+    for i in range(len(s)):
+        l, r=i, i
+        while l>=0 and r<len(s) and s[l]==s[r]:
+            if r-l+1>cnt:
+                res=s[l:r+1]
+                cnt=r-l+1
+            l-=1
+            r+=1
+            
+        l, r = i, i+1
+        while l>=0 and r<len(s) and s[l]==s[r]:
+            if r-l+1>cnt:
+                res=s[l:r+1]
+                cnt=r-l+1
+            l-=1
+            r+=1
+    return res
+
 if __name__ == '__main__':
-    s = 'abcbaa'
-    print(f'Substring with k distinct count: {count_sustrings(s, 3)}')
+    s = 'babad'
+    print(f'Longest Palindromic substring: {longestPalindrome(s)}')
