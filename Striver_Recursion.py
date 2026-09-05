38dfd84f4f92f2bab7bacdbc67d75a798b4621d9
Shourya Gupta <shouryaagupta0@gmail.com>
2026-09-05 23:19:48 +0530

    Word Search

--- Striver_Recursion.py
+++ Striver_Recursion.py
@@ -180,6 +180,40 @@ def letterCombinations(digits):
 
     helper(0)
     return result
+    
+# Word Search
+def exist(board, word):
+    rows = len(board)
+    cols = len(board[0])
+
+    def dfs(i, j ,idx):
+        if idx == len(word):
+            return True
+
+        if i < 0 or j < 0 or i >= rows or j >= cols or board[i][j] != word[idx]:
+            return False
+
+        temp = board[i][j]
+        board[i][j] = '#'
+
+        found = (
+            dfs(i + 1, j, idx + 1) or
+            dfs(i - 1, j, idx + 1) or
+            dfs(i, j - 1, idx + 1) or
+            dfs(i, j + 1, idx + 1)
+        )
+
+        board[i][j] = temp
+
+        return found
+
+    for i in range(rows):
+        for j in range(cols):
+            if dfs(i, j, 0):
+                return True
+    return False
 
 if __name__ == '__main__':
-    print(letterCombinations('23'))
+    board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
+    word = "ABCCED"
+    print(exist(board, word))
