4f47523c910fdfcba09f0e387fa2c1f79fad933d
Shourya Gupta <shouryaagupta0@gmail.com>
2026-09-09 01:12:42 +0530

    Solved N-Queens

--- Striver_Recursion.py
+++ Striver_Recursion.py
@@ -213,7 +213,30 @@ def exist(board, word):
                 return True
     return False
 
+def solveQueens(n):
+    def solve(col, board, n, leftRow, upperDiagonal, lowerDiagonal, ans):
+        if col == n:
+            ans.append(["".join(row)for row in board])
+            return
+
+        for row in range(n):
+            if leftRow[row] == 0 and lowerDiagonal[row + col] == 0 and upperDiagonal[n - 1 + col - row] == 0:
+
+                board[row][col] = 'Q'
+                leftRow[row] = lowerDiagonal[row + col] = upperDiagonal[n - 1 + col - row] = 1
+
+                solve(col + 1, board, n, leftRow, upperDiagonal, lowerDiagonal, ans)
+
+                board[row][col] = '.'
+                leftRow[row] = lowerDiagonal[row + col] = upperDiagonal[n - 1 + col - row] = 0
+
+    ans = []
+    board = [['.' for _ in range(n)] for _ in range(n)]
+    leftRow = [0] * n
+    upperDiagonal = [0] * (2 * n - 1)
+    lowerDiagonal = [0] * (2 * n - 1)
+    solve(0, board, n, leftRow, upperDiagonal, lowerDiagonal, ans)
+    return ans
+
 if __name__ == '__main__':
-    board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
-    word = "ABCCED"
-    print(exist(board, word))
+    print(solveQueens(4))
