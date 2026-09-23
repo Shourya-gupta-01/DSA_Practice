49c011bfc6f1650f17a32290ccd8befbe685323b
Shourya <shouryaagupta0@gmail.com>
2026-09-23 20:12:04 +0530

    Solve Check n is Power of 2 or Not

--- Striver_Bit_Manipulation.py
+++ Striver_Bit_Manipulation.py
@@ -1,8 +1,14 @@
+# Check the Ith bit
 def checkIthBit(n: int, i: int) -> bool:
     return n & (1 << i) != 0
 
+# Check if the number is odd
 def checkOdd(n: int) -> bool:
     return n & 1 == 1
 
+# Check if the number is power of 2 or not
+def checkPower2(n: int) -> bool:
+    return n & n - 1 == 0
+
 if __name__ == '__main__':
-    print(checkOdd(13))
+    print(checkPower2(8))
