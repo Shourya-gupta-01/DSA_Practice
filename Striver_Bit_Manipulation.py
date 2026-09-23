23db118733331a21fe12ef7a5614084b1620fc91
Shourya <shouryaagupta0@gmail.com>
2026-09-23 20:06:03 +0530

    Solve Check Odd or Not.

--- Striver_Bit_Manipulation.py
+++ Striver_Bit_Manipulation.py
@@ -1,5 +1,8 @@
 def checkIthBit(n: int, i: int) -> bool:
     return n & (1 << i) != 0
 
+def checkOdd(n: int) -> bool:
+    return n & 1 == 1
+
 if __name__ == '__main__':
-    print(checkIthBit(16, 3))
+    print(checkOdd(13))
